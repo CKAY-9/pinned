5f558ab65515fa54299bd1b5b6328c3810bac187
Cameron <53030585+CKAY-9@users.noreply.github.com>
2024-02-02 11:06:12 -0800

    Update page.tsx

--- pinned-frontend/src/app/about/page.tsx
+++ pinned-frontend/src/app/about/page.tsx
@@ -2,6 +2,7 @@ import { getUserFromToken } from "@/api/user/user";
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const generateMetadata = (): Metadata => {
   return {
@@ -16,9 +17,30 @@ const AboutPage = async () => {
   return (
     <>
       <Header user={user} />
-      <main className="container">
-        <h1>About Pinned</h1>
-        <span>Coming soon...</span>
+      <main className="container" style={{"gap": "3rem"}}>
+        <section>
+          <h1>About Pinned</h1>
+          <p>
+            Pinned is a social platform meant for creators, creatives, and anyone who wants to share things that find interesting.
+            Pinned is dedicated to making sure that posts shared on the platform are found via searching, algorithmic sorting, and general
+            post finding. Users can upload images through posts, create collections, post comments, and vote on posts and collections.
+          </p>
+        </section>
+        <section>
+          <h2>Behind the Scenes</h2>
+          <p>
+            Pinned is comprised of three main services or components:
+          </p>
+          <ul>
+            <li>pinned-frontend: The primary frontend or client-side interface for Pinned. Written in TypeScript using NextJS</li>
+            <li>pinned-backend: The primary API and server-side service for Pinned. Written in Rust using Actix and Diesel.</li>
+            <li>pinned-cdn: The primary CDN (content delivery network) for images and other static files for Pinned. Written in Python using Flask.</li>
+          </ul>
+          <p>
+            The official repository for Pinned can be found on <Link href="https://github.com/CKAY-9/pinned">GitHub</Link>, along with licenses and more specfics on
+            technical details of Pinned.
+          </p>
+        </section>
       </main>
       <Footer user={user} />
     </>
